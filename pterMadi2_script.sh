2d1085351dd50e2d66c971c73cb8546f9a34d81c
crcardenas <36632979+crcardenas@users.noreply.github.com>
2023-02-08 20:47:32 +0100

    added exon GFF names

--- pterMadi2_script.sh
+++ pterMadi2_script.sh
@@ -22,13 +22,16 @@ conda activate sam-bam-bedtools
 bedtools sort -i Pterostichus_madidus-GCA_911728475.1-2021_12-genes.gff -g pterMadi2.genomefile > pterMadi2.sorted.gff
 
 # generate intergenic regions
-awk '$3 == "gene" {print $1 "\t" $4-1 "\t" $5}' pterMadi2.sorted.gff > pterMadi2.sorted.genes.bed
+#awk '$3 == "gene" {print $1 "\t" $4-1 "\t" $5}' pterMadi2.sorted.gff > pterMadi2.sorted.genes.bed
+awk '$3 == "gene" {print $0}' pterMadi2.sorted.gff > pterMadi2.sorted.genes.gff
 
 # excise intergenic regions from gene features
-bedtools complement -i pterMadi2.sorted.genes.bed -g pterMadi2.genomefile > pterMadi2.sorted.intergenic.bed
+bedtools complement -i pterMadi2.sorted.genes.gff -g pterMadi2.genomefile > pterMadi2.sorted.intergenic.bed
 
 # extract exons
-awk '$3 == "exon" {print $1 "\t" $4-1 "\t" $5}' pterMadi2.sorted.gff > pterMadi2.sorted.exons.bed
+# need two files here, one with your typical bed files, and another with just exon in gff format. The gff format will allow for exon identifiers to be included in those gff files
+awk '$3 == "exon" {print $1 "\t" $4-1 "\t" $5}' pterMadi2.sorted.gff > pterMadi2.sorted.exons.bed # this will be so we can map intergenic regions
+awk '$3 == "exon" {print $0}' pterMadi2.sorted.gff > pterMadi2.sorted.exons.gff # this ensures that we have a gff file to give intersect file exon names for later workflow
 
 # join and sort
 bedtools sort -i <(cat pterMadi2.sorted.intergenic.bed pterMadi2.sorted.exons.bed) -g pterMadi2.genomefile > pterMadi2.intergenic-and-exons.sorted.bed
@@ -41,24 +44,24 @@ bedtools complement -i pterMadi2.intergenic-and-exons.sorted.bed -g pterMadi2.ge
 bwa index pterMadi2_newheader.fna
 
 # align probes to indexed genome
-bwa mem pterMadi2_newheader.fna Adephaga_2.9Kv1_UCE-Probes.fasta > pterMadi2.sam
+bwa mem pterMadi2_newheader.fna Adephaga_2.9Kv1_UCE-Probes.fasta > pterMadi2-probes.sam
 
 # convert sam to bam
-samtools view -h -b -S pterMadi2.sam > pterMadi2_mem-probes.bam
+samtools view -h -b -S pterMadi2-probes.sam > pterMadi2-probes.mem.bam
 
 # sort bam file
-samtools sort pterMadi2_mem-probes.bam -o pterMadi2_mem-probes.sorted.bam
+samtools sort pterMadi2-probes.mem.bam -o pterMadi2-probes.mem.sorted.bam
 
 # extract sequences that map against the genome
-samtools view -b -F 4 pterMadi2_mem-probes.sorted.bam > pterMadi2_mem-probes.sorted.mapped.bam
+samtools view -b -F 4 pterMadi2-probes.mem.sorted.bam > pterMadi2-probes.mem.sorted.mapped.bam
 
 # create bed file
-bedtools bamtobed -i pterMadi2_mem-probes.sorted.mapped.bam > pterMadi2_mem-probes.sorted.mapped.bed
+bedtools bamtobed -i pterMadi2-probes.mem.sorted.mapped.bam > pterMadi2-probes.mem.sorted.mapped.bed
 
 # get intersect of probes and genomic features
-bedtools intersect -a pterMadi2_mem-probes.sorted.mapped.bed \
+bedtools intersect -a pterMadi2-probes.mem.sorted.mapped.bed \
     -b pterMadi2.sorted.intergenic.bed \
     pterMadi2.sorted.introns.bed \
-    pterMadi2.sorted.exons.bed  \
-    -names intergenic introns exons \
+    pterMadi2.sorted.exons.gff  \
+    -names intergenic intron exon \
     -wb > Adephaga2.9-pterMadi2.intersect
