0e7ab0bcec7c3009fe07071328a0875e956fda6b
crcardenas <36632979+crcardenas@users.noreply.github.com>
2023-08-25 13:30:31 +0200

    Update script_synteny.R
    update R script. runs with shell script, but needs broken up into two scripts.

--- script_synteny.R
+++ script_synteny.R
@@ -1,7 +1,7 @@
 #### script_synteny.R ####
 ## Cody Raul Cardenas 
 ## 2023.08 
-## Modified from ? Jérémy Gauthier's synteny_script.R
+## Modified from Joana Meier's synteny_script.R
 
 # to be run with a bash script like this
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -34,8 +34,9 @@ if (length(args) < 2) {
   stop("Two argument must be supplied as input.\n", call. = FALSE)
 } else {
   print(paste0("Genome input:  ", args[1]))
-  print(paste0("Genome input:  ",))
-  print("")
+  print(paste0("Genome input:  ", args[2]))
+  print("Saving file")
+  print("Process output in excel or equivelant")
 }
 
 # load library
@@ -56,139 +57,118 @@ sp1_bedfile <- read.table(c(paste0(species1,".ortho-loci.bed")))
 colnames(sp1_bedfile) <- c(paste0("chr_",species1),
                            paste0("start_",species1),
                            paste0("end_",species1),
-                           paste0("loci_",species1))
+                           paste0("loci"))
 
 sp2_bedfile <- read.table(c(paste0(species2,".ortho-loci.bed")))
 colnames(sp2_bedfile) <- c(paste0("chr_",species2),
                            paste0("start_",species2),
                            paste0("end_",species2),
-                           paste0("loci_",species2))
+                           paste0("loci"))
 
 # genome fai index files
 sp1_fai <- read.table(c(paste0("../",species1,".fna.fai")))
 rownames(sp1_fai) <- sp1_fai$V1
-# add column names?
 
 sp2_fai <- read.table(c(paste0("../",species2,".fna.fai")))
 rownames(sp2_fai) <- sp2_fai$V1
-# add column names?
 
 #### Manipulate data ####
+# retain only chromosomes
+sp1_fai <- subset(sp1_fai, nchar(as.character(V1)) <= 3)
+sp2_fai <- subset(sp2_fai, nchar(as.character(V1)) <= 3)
+
+# remove any UCE loci from these chromosomes
+# make a list to check if loci on scaffolds
+sp1_exclude <- subset(sp1_bedfile, nchar(sp1_bedfile[,1]) > 3)[,4]
+sp2_exclude <- subset(sp2_bedfile, nchar(sp2_bedfile[,1]) > 3)[,4]
+# create a list
+exclude <- paste0(sp1_exclude,sp2_exclude)
+# remove those loci on those scaffolds
+sp1_bedfile <- sp1_bedfile[ ! (sp1_bedfile[,4] %in% exclude) , ]
+sp2_bedfile <- sp2_bedfile[ ! (sp2_bedfile[,4] %in% exclude) , ]
+
 # Order according to chromosome names
 sp1_fai <- sp1_fai[mixedsort(rownames(sp1_fai),decreasing = F),]
 sp2_fai <- sp2_fai[mixedsort(rownames(sp2_fai),decreasing = F),]
 
-# remove short chromosomes?
+# add space between the chromosomes for visualisation
+sp1_fai$add<-cumsum(c(0,sp1_fai$V2[-length(sp1_fai$V2)]+11000000))
+sp2_fai$add<-cumsum(c(0,sp2_fai$V2[-length(sp2_fai$V2)]+7700000))
 
-#### scripting notes ####
-# use some variable in file path
-# dir <- file.path("some", "path")
-# bla <- file.path("some", "directory")
-# files <- c("file1.R", "file2.exe")
-# 
-# file.path(dir, bla, files)
-#
-# results in:
-# [1] "some/path/some/directory/file1.R"   "some/path/some/directory/file2.exe"
-# 
-# might be able to call paste0(c()) within a function. instead of:
-# workingdir <- paste0("./",species1,"_",species2,"/")
-# *maybe*
-# myfunciton(directory=paste0("./",species1,"_",species2,"/"), other_arg=0, ...)
+# add color for plotting
+sp1_fai$col<-paletteMartin[1:nrow(sp1_fai)]
+
+# WORK ON THESE CODES
+# FOR TESTING swap orientation of chromosomes in species one
+chrLengths_sp1 <- as.data.frame(aggregate(sp1_bedfile[,3] ~ sp1_bedfile[,1],
+                                      FUN = max))
+colnames(chrLengths_sp1) <- c(paste0("chr_",species1),
+                           paste0("len_",species1))
+for ( chr in chrLengths_sp1[,1] ) {
+  sp1_bedfile[sp1_bedfile[,1]==chr,2:3] <- chrLengths_sp1[chrLengths_sp1[,1]==chr,2] - sp1_bedfile[sp1_bedfile[,1]==chr,2:3]
+}
+
+# Add a column with additive positions
+sp1_bedfile$V5 <- rowMeans(cbind(sp1_bedfile[,2], sp1_bedfile[,3])) + sp1_fai[sp1_bedfile[,1], "add"]
+colnames(sp1_bedfile) <- c(paste0("chr_",species1),
+                          paste0("start_",species1),
+                          paste0("end_",species1),
+                          paste0("loci"),
+                          paste0("addPos_",species1))
+  
+sp2_bedfile$V5 <- rowMeans(cbind(sp2_bedfile[,2], sp2_bedfile[,3])) + sp2_fai[sp2_bedfile[,1], "add"]
+colnames(sp2_bedfile) <- c(paste0("chr_",species2),
+                           paste0("start_",species2),
+                           paste0("end_",species2),
+                           paste0("loci"),
+                           paste0("addPos_",species2))
+
+# Merge the two gene coordinate tables
+loci <- merge(sp1_bedfile, sp2_bedfile, by="loci")
+loci2 <- loci[order(loci[,6], loci[,7]),]
+
+# MAY NEED TO WRITE TWO SCRIPTS
+# ONE TO GET THIS FILE (loci1, loci2)
+# THEN THAT FILE NEEDS TO BE MANUALLY CURRATED IN EXCEL or EQUIVELANT
+
+#    Write out and get the correct order in Excel
+#    write.table(busco,file="D:/Dropbox/Ithomiines/ReferenceGenomes/curatedMelinaeaGenomes/busco.comparison.txt")
+#    busco<-read.table("D:/Dropbox/Ithomiines/ReferenceGenomes/curatedMelinaeaGenomes/busco.comparison.txt",header=T)
 
 
-#### Jeremy's code ####
-# !!! if commented out that means I've added that to my modified script
-
-
-   # Read in the Genome fasta index files
-  
-   # Genome fasta index files
-   # fai_sali<-read.table("GCA_944039245.1_icNebSali1.1_genomic.fna.fai")
-   # fai_brev<-read.table("GCA_944738965.1_icNebBrev1.1_genomic.fna.fai")
-  
-   # Add rownames for ordering the chromosomes:
-   # rownames(fai_sali)<-fai_sali$V1
-   # rownames(fai_brev)<-fai_brev$V1
-  
-   # Remove tiny scaffolds
-   #fai_sali<-fai_sali[!grepl(fai_sali$V1,pattern="unloc|scaffold|:"),]
-   #fai_brev<-fai_brev[!grepl(fai_brev$V1,pattern="unloc|scaffold|:"),]
-  
-   # # Order according to chromosome names
-   # require(gtools)
-   # fai_sali<-fai_sali[mixedsort(rownames(fai_sali),decreasing = F),]
-   # fai_brev<-fai_brev[mixedsort(rownames(fai_brev),decreasing = F),]
-  
-   # # Order according to M. menophilus chromosome numbers
-   #fai_meno<-fai_meno[c("SUPER_9","SUPER_16","SUPER_12","SUPER_10","SUPER_5","SUPER_4","SUPER_13","SUPER_7","SUPER_14","SUPER_3","SUPER_8","SUPER_17","SUPER_15","SUPER_19","SUPER_1","SUPER_18","SUPER_11","SUPER_2","SUPER_6","SUPER_20","SUPER_Z"),]
-  
-  # OK porquoi? why add length?
-   # Add additive positions (to plot chromosomes next to each other)
-   fai_sali$add<-cumsum(c(0,fai_sali$V2[-length(fai_sali$V2)]+11000000))
-   fai_brev$add<-cumsum(c(0,fai_brev$V2[-length(fai_brev$V2)]+7700000))
-  
-  # OK porquoi? for... only one??
-   # Add a colour column where each chromosome has a different colour using a colourblind friendly palette
-   require(colorBlindness)
-   fai_sali$col<-paletteMartin[1:nrow(fai_sali)]
-  
-   # # Read in BUSCO gene coordinates
-   # busco_brev<-read.table("C_NebBrev.bed")
-   # busco_sali<-read.table("C_NebSali.bed")
-   # 
-   # # Rename the columns
-   # names(busco_brev)<-c("chr_brev","start_brev","end_brev","gene")
-   # names(busco_sali)<-c("chr_sali","start_sali","end_sali","gene")
-  
-   # remove tiny scaffolds from the BUSCO gene set
-   #busco_mars<-busco_mars[!grepl(busco_mars$chr_mars,pattern="unloc|scaffold|:"),]
-   #busco_meno<-busco_meno[!grepl(busco_meno$chr_meno,pattern="unloc|scaffold|:"),]
-  
-   # swap orientation of some M. menophilus chromosomes to better match the M. marsaeus chr
-   chrLengths<-as.data.frame(aggregate(busco_brev$end_brev ~ busco_brev$chr_brev,FUN = max))
-   names(chrLengths)<-c("chr_brev","length")
-   for(chr in chrLengths$chr_brev){
-     busco_brev[busco_brev$chr_brev==chr,2:3]<-chrLengths[chrLengths$chr_brev==chr,2]-busco_brev[busco_brev$chr_brev==chr,2:3]
-   }
-   
-   # Add a column with additive positions
-   busco_sali$addPos_sali<-rowMeans(cbind(busco_sali$start_sali,busco_sali$end_sali))+fai_sali[busco_sali$chr_sali,"add"]
-   busco_brev$addPos_brev<-rowMeans(cbind(busco_brev$start_brev,busco_brev$end_brev))+fai_brev[busco_brev$chr_brev,"add"]
-  
-  
-   # Merge the two gene coordinate tables
-   busco<-merge(busco_sali,busco_brev,by="gene")
-   busco2<-busco[order(busco$chr_sali,busco$start_sali),]
-  
-   # Write out and get the correct order in Excel
-   # write.table(busco,file="D:/Dropbox/Ithomiines/ReferenceGenomes/curatedMelinaeaGenomes/busco.comparison.txt")
-   # busco<-read.table("D:/Dropbox/Ithomiines/ReferenceGenomes/curatedMelinaeaGenomes/busco.comparison.txt",header=T)
-  
-
-# ! Consider saving the R script files
-
+#### plot ####
+#pdf(file=c(paste0(species1,"_",species2,"_syntenyplot.pdf")))
    # Plot the gene coordinates against each other
    par(mfrow=c(1,1),mar=c(4,4,1,1),mgp=c(1.7,0.5,0),xaxs="i",yaxs="i")
-   plot(busco2$addPos_sali,busco2$addPos_brev,xaxt="n",yaxt="n",
-        col=as.integer(as.factor(busco2$chr_sali)),
+   plot(loci2[,5],
+        loci2[,9],
+        xaxt="n",
+        yaxt="n",
+        col=as.integer(as.factor(loci2[,6])),
         pch=19,cex=0.5,
-        xlab=expression(italic("Nebria brev")~"chromosomes"),
-        ylab=expression(italic("Nebria sali")~"chromosomes"))
+        xlab=c(paste0(species1," chromosomes")),
+        ylab=c(paste0(species2," chromosomes")))
   
-   plot(busco$addPos_sali,busco$addPos_brev,xaxt="n",yaxt="n",
-        col=as.integer(as.factor(busco$chr_sali)),
-        pch=19,cex=0.5,xlab=expression(italic("Nebria brev")~"chromosomes"),
-        ylab=expression(italic("Nebria sali")~"chromosomes"))
-  
-  
+   # plot(loci[,9],
+   #      loci[,5],
+   #      xaxt="n",yaxt="n",
+   #      col=as.integer(as.factor(loci2[,6])),
+   #      pch=19, cex=0.5, 
+   #      xlab=c(paste0(species1," chromosomes")),
+   #      ylab=c(paste0(species2," chromosomes")))
+   # 
   
    # Add lines showing the ends of the chromosomes
-   abline(v=fai_sali$add[-1][!grepl(fai_sali$V1,pattern="unloc")],col="grey")
-   abline(h=fai_brev$add[-1],col="grey")
-  
-   # Label the axes
-   axis(1,fai_sali$add[!grepl(fai_sali$V1,pattern="unloc")]+
-          fai_sali$V2[!grepl(fai_sali$V1,pattern="unloc")]/2,
-        fai_sali$V6[!grepl(fai_sali$V1,pattern="unloc")],line = -0.2,tick = F)
-   axis(2,fai_brev$add+fai_brev$V2/2,fai_brev$V6,las=2,tick = F)
+   abline(v=sp1_fai$add[-1][!grepl(sp1_fai$V1,pattern="unloc")],col="grey")
+   abline(h=sp2_fai$add[-1],col="grey")
+
+  # Label the axes
+   # axis(2, sp1_fai[,1], line = -0.2, tick = F)
+   # axis(1, sp2_fai[,1], las = 2, tick = F)
+   
+# not sure what this did...
+   # axis(1,sp2_fai$add[!grepl(sp2_fai$V1, pattern="unloc")]+
+   #        sp2_fai$V2[!grepl(sp2_fai$V1, pattern="unloc")]/2,
+   #      sp2_fai$V6[!grepl(sp2_fai$V1, pattern="unloc")], line = -0.2, tick = F)
+   # axis(2, sp1_fai$add+sp1_fai$V2/2, sp1_fai$V6,las=2,tick = F)
+#dev.off()
